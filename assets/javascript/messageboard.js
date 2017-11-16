8c693fb8c3c63381abab374c0144dcadd218844d
Michael Klingelberg <Mike@dhcp-v089-035.mobile.uci.edu>
2017-11-15 20:10:19 -0800

    added firebase to index.html, included assets folder and messageboard.js

---
+++ assets/javascript/messageboard.js
@@ -0,0 +1,20 @@
+// call those top 5 posts from firebase, display top 5 posts to carousel.
+
+// make button to submit review or post clickable
+// .on click make page move to submit form
+
+// call all reviews or post from firebase and display them to
+// make sure only first letter of last name displays add period
+// make sure that email address or phone number doesn't display
+// display linkedin, facbook and any other site values as links
+// 
+
+// code out the giphy API
+// make the one or two words call the giphy API value of those words
+// display the value of the giphy pulled.
+// allow for user to shuffle to the next giphy.
+// make sure that only one giphy displays and if you do add button it replaces.
+
+// make submit button clickable and push all data to firebase.
+
+console.log(config);
