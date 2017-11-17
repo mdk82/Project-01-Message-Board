256a368c79179dd4e74c90aedb390d43b23d5796
Michael Klingelberg <Mike@Michaels-MacBook-Air.local>
2017-11-16 19:39:20 -0800

    minor fixes

--- assets/javascript/messageboard.js
+++ assets/javascript/messageboard.js
@@ -17,4 +17,41 @@
 
 // make submit button clickable and push all data to firebase.
 
-console.log(config);
+
+     // Initialize Firebase
+var config = {
+	apiKey: "AIzaSyC6FucDpOVW_DqaN3p4F-oc_Vmnc6uBVMY",
+    authDomain: "message-board-project-dc938.firebaseapp.com",
+    databaseURL: "https://message-board-project-dc938.firebaseio.com",
+    projectId: "message-board-project-dc938",
+    storageBucket: "message-board-project-dc938.appspot.com",
+    messagingSenderId: "50008848295"
+};
+      
+    firebase.initializeApp(config);
+
+    
+
+$( window ).on( "load", function() {
+        console.log( "window loaded" );
+
+
+$('#submit-message').on('click', function() {
+
+});
+
+$('.display-name').text('John Doe')
+$('.display-location').text('location API call')
+$('.display-message').text('message that user will submit')
+$('.display-timeDate').text('date and time')
+$('.display-gif').text('giphy action here')
+
+
+var giphyWords = [];
+var 
+
+
+});
+
+
+
