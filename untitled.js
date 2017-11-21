6d4c5efd514bc229a001669c31c48c8b10fdd7c6
Danilo Kim <danilo@dhcp-v111-108.mobile.uci.edu>
2017-11-20 19:45:03 -0800

    shuttle button working

--- untitled.js
+++ untitled.js
@@ -2,6 +2,9 @@
 $(document).ready(function(){
 console.log("ready!");
 
+// Storing counter as 0
+var counter = 0;
+
 
 // Document ready ~ in click giphy-button
 $(document).on('click','#giphy-button',function(){
@@ -12,7 +15,7 @@ $(document).on('click','#giphy-button',function(){
 		console.log(gifWord)
 
 		//Storing our giphy API URL for a ramdon cata image
-		var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+ gifWord +'&api_key=RGGd6MKHcHQW7kQvmNIEHxcD0AyG9j2Q&limit=1';
+		var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+ gifWord +'&api_key=RGGd6MKHcHQW7kQvmNIEHxcD0AyG9j2Q&limit=10';
 
 		// Ajax GET request to our queryURL
 		$.ajax({
@@ -21,33 +24,70 @@ $(document).on('click','#giphy-button',function(){
 		})
 		// After the data from the AJAX request 
 		.done(function(response) {
-			
-			$("#giphy_word").empty();
+			console.log(response);
 
 			// Storing variables
 			var results = response.data;
 
-			// Looping over every result item
-			for (var i = 0; i < results.length; i++) {
-			
 				// Creating a div with the class "item"
 				var gifDiv = $("<div class='item'>");
 
 				// Creating an image tag
-
 				var feelingImage = $('<img>');
 
 				// Setting the feelingImage a new attribute
-				feelingImage.attr("src", results[i].images.fixed_height.url);
-				feelingImage.attr("alt", "your opinion is pointless");
+				feelingImage.attr("src", results[0].images.fixed_height.url);
 
-				// Appending 
-				gifDiv.append(feelingImage);
+				// Shows giphy on screen
+				gifDiv.html(feelingImage);
 
-				// Prepending the gifDiv
-				$(".users-giphy").prepend(gifDiv);
-				// Prepend the entire giphy
-			}	
+				$(".users-giphy").html(gifDiv);
 		});
 	});
+
+$(document).on('click','#shuffle-button',function(){
+		console.log("button clicked")
+
+		// Pull the users' input to be used
+		var gifWord = $("#giphy_word").val().trim();
+		console.log(gifWord)
+
+		//Storing our giphy API URL for a ramdon cata image
+		var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+ gifWord +'&api_key=RGGd6MKHcHQW7kQvmNIEHxcD0AyG9j2Q&limit=10';
+
+		// Ajax GET request to our queryURL
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		})
+		// After the data from the AJAX request 
+		.done(function(response) {
+			console.log(response);
+		
+			counter++;
+			// Storing variables
+			var results = response.data;
+			console.log(results);
+				
+				if (counter === 10){
+					counter = 0;
+				} 
+				// Creating a div with the class "item"
+				var gifDiv = $("<div class='item'>");
+
+				// Creating an image tag
+				var feelingImage = $('<img>');
+
+				// Setting the feelingImage a new attribute
+				feelingImage.attr("src", results[counter].images.fixed_height.url);
+
+				// Shows giphy on screen
+				gifDiv.html(feelingImage);
+
+				$(".users-giphy").html(gifDiv);
+				// Prepend the entire giphy
+				console.log(counter);
+		});
+	});
+
 });
