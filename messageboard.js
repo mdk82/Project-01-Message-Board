5d06c2a6e1da968918034c4039ec111db1e2365d
Michael Klingelberg <Mike@Michaels-MacBook-Air.local>
2017-11-22 22:47:03 -0800

    added functionality to JS file and changes to index2

--- messageboard.js
+++ messageboard.js
@@ -1,26 +1,9 @@
-// call those top 5 posts from firebase, display top 5 posts to carousel.
 
-// make button to submit review or post clickable
-// .on click make page move to submit form
-
-// call all reviews or post from firebase and display them to
-// make sure only first letter of last name displays add period
-// make sure that email address or phone number doesn't display
-// display linkedin, facbook and any other site values as links
-// 
-
-// code out the giphy API
-// make the one or two words call the giphy API value of those words
-// display the value of the giphy pulled.
-// allow for user to shuffle to the next giphy.
-// make sure that only one giphy displays and if you do add button it replaces.
-
-// make submit button clickable and push all data to firebase.
-// $( window ).on( "load", function() {
-//         console.log( "window loaded" );
 
 // Initialize Firebase //
 // =================== //
+
+console.log('working')
 
 var config = {
 	apiKey: "AIzaSyC6FucDpOVW_DqaN3p4F-oc_Vmnc6uBVMY",
@@ -41,17 +24,101 @@ var lastName = "";
 var lastInt = "";
 var email = "";
 var phone = "";
-var linkedin = "";
-var portfolio = "";
+var siteLink = "";
 var comment = "";
 
-var giphyDisplay = "";
+var giphyChosen;
+var displayName = "";
+
+// Button "LEAVE A MESSAGE" anchor to form //
+// ======================================= //
+
+$('#anchor-button').on('click', function() {
+	location.href = "#first-name";
+});
+
+
+// Storing counter as 0
+var counter = 0;
+// Document ready ~ in click giphy-button
+$(document).on('click','#giphy-button',function() {
+	event.preventDefault();
+        console.log("button clicked")
+        // Pull the users' input to be used
+        var gifWord = $("#giphy-word").val().trim();
+        console.log(gifWord)
+        //Storing our giphy API URL for a ramdon cata image
+        var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+ gifWord +'&api_key=RGGd6MKHcHQW7kQvmNIEHxcD0AyG9j2Q&limit=10';
+        // Ajax GET request to our queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After the data from the AJAX request 
+        .done(function(response) {
+            console.log(response);
+            // Storing variables
+            var results = response.data;
+                // Creating a div with the class "item"
+                var gifDiv = $("<div class='item'>");
+                // Creating an image tag
+                var feelingImage = $('<img>');
+                // Setting the feelingImage a new attribute
+                feelingImage.attr("src", results[0].images.fixed_height.url);
+                giphyChosen = results[0].images.fixed_height.url;
+                // Shows giphy on screen
+                gifDiv.html(feelingImage);
+                $(".users-giphy").html(gifDiv);
+
+                console.log(giphyChosen);
+        });
+    });
+$(document).on('click','#shuffle-button',function() {
+	event.preventDefault();
+        console.log("button clicked")
+        // Pull the users' input to be used
+        var gifWord = $("#giphy-word").val().trim();
+        console.log(gifWord)
+        //Storing our giphy API URL for a ramdon cata image
+        var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+ gifWord +'&api_key=RGGd6MKHcHQW7kQvmNIEHxcD0AyG9j2Q&limit=10';
+        // Ajax GET request to our queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After the data from the AJAX request 
+        .done(function(response) {
+            console.log('response' + response);
+        
+            counter++;
+            // Storing variables
+            var results = response.data;
+            console.log('results' + results);
+                
+                if (counter === 10){
+                    counter = 0;
+                } 
+                // Creating a div with the class "item"
+                var gifDiv = $("<div class='item'>");
+                // Creating an image tag
+                var feelingImage = $('<img>');
+                // Setting the feelingImage a new attribute
+                feelingImage.attr("src", results[counter].images.fixed_height.url);
+                giphyChosen = results[counter].images.fixed_height.url
+                // Shows giphy on screen
+                gifDiv.html(feelingImage);
+                $(".users-giphy").html(gifDiv);
+                // Prepend the entire giphy
+              
+                console.log(giphyChosen);
+        });
+    });
 
 
 // On click "submit-message" push all form values to variables //
-// ================================================== // 
+// =========================================================== // 
 
-$('#submit-message').on('click', function() {
+$('#submit-button').on('click', function() {
 
 	// test
 	console.log('working');
@@ -62,11 +129,14 @@ $('#submit-message').on('click', function() {
 		lastName = $('#last-name').val().trim();
 		email = $('#input-email').val().trim();
 		phone = $('#input-telephone').val().trim();
-		linkedin = $('#input-linkedin').val().trim();
-		portfolio = $('#input-portfolio').val().trim();
+		siteLink = $('#input-link').val().trim();
 		comment = $('#input-message').val().trim();
 
 		lastInt = lastName.charAt(0);
+
+		displayName = firstName + " " + lastInt;
+
+		console.log(displayName);
 
 	// Push variable data to firebase //
 	// ============================== //
@@ -76,70 +146,77 @@ $('#submit-message').on('click', function() {
 		first:firstName,
 		last:lastName,
 		lastInt:lastInt,
+		displayName:displayName,
 		email:email,
 		phone:phone,
-		linkedin:linkedin,
-		portfolio:portfolio,
+		siteLink:siteLink,
 		comment:comment,
+		giphy:giphyChosen,
 		dateAdded:firebase.database.ServerValue.TIMESTAMP
 
 	})
 
-		// test variables values //
+	$('.name__switch').click(function() {
+    if ($('.myCheckbox').prop('checked', true)) {
+    	$('.display-name').html("Ghost Name");
+    }
+
+	else if	($('.myCheckbox').prop('checked', false)) {
+		$('.display-name').html(snapshot.val().displayName);
+	}
+});
+
+
+		// test variable values //
 		console.log(firstName);
 		console.log(lastName);
 		console.log(email);
 		console.log(phone);
-		console.log(linkedin);
-		console.log(portfolio);
+		console.log(siteLink);
 		console.log(comment);
 		console.log(lastInt);
+		console.log(giphyChosen);
 
 	// Make all form fields go back to blank values//
-	// ===================================== //
+	// =========================================== //
+
 	$('#first-name').val("");
 	$('#last-name').val("");
 	$('#input-email').val("");
 	$('#input-telephone').val("");
-	$('#input-linkedin').val("");
-	$('#input-portfolio').val("");
+	$('#input-link').val("");
 	$('#input-message').val("");
+	$('#giphy-word').val("");
+	$(".users-giphy").empty();
 });
 
-	firebase.database().ref().on("child_added", function(snapshot){
 
-		// if ( hide your name ) {
-		// 	put Ghost name in for val of first and last name.
-		// } else {
-		// 	do other display of val
-		// }
+	firebase.database().ref().orderByChild("dataAdded").on("child_added", function(snapshot) {
+	
+	var fireGif = snapshot.val().giphy;
+	var $gifDiv = $('<img src="' + fireGif + '" alt="Mountain View">')
+	$('.display-name').html(snapshot.val().displayName);
+	$('.display-message').html('" ' +snapshot.val().comment + ' "');
+	$('.display-gif').html($gifDiv);
 
-		$('.display-gif').append("<div>" + snapshot.val().first + "</div>");
-		$('.display-gif').append("<div>" + snapshot.val().lastInt + "</div>");
-		$('.display-gif').append("<div>" + snapshot.val().email + "</div>");
-		$('.display-gif').append("<div>" + snapshot.val().phone + "</div>");
-		$('.display-gif').append("<div>" + snapshot.val().linkedin + "</div>");
-		$('.display-gif').append("<div>" + snapshot.val().portfolio + "</div>");
-		$('.display-gif').append("<div>" + snapshot.val().comment + "</div>");
-		$('.display-gif').append("<div" + snapshot.val().dateAdded + "</div>");
-		$('.display-gif').append("<hr>");
+// Button to view profolio site or linkedin //
+// ======================================== //
+
+	$('.message__url').on('click', function() {
+	window.open(snapshot.val().siteLink);
+});
+
 	});
 
+$('#reset-button').on('click', function() {
 
-	// Placeholder data for test //
-	// firebase.database().ref().orderByChild("dataAdded").on("child_added", function(snapshot) {
-
-	// $('.display-name').html(snapshot.val().first);
-	// $('.display-location').html(snapshot.val().last);
-	// $('.display-message').html(snapshot.val().email);
-	// $('.display-timeDate').html(snapshot.val().phone);
-	// $('.display-gif').html(snapshot.val().linkedin);
-	// $('.display-gif').html(snapshot.val().portfolio);
-	// $('.display-gif').html(snapshot.val().comment);
-	// $('.display-gif').html(snapshot.val().dateAdded);
-
-	// });
-
-
-
+	$('#first-name').val("");
+	$('#last-name').val("");
+	$('#input-email').val("");
+	$('#input-telephone').val("");
+	$('#input-link').val("");
+	$('#input-message').val("");
+	$('#giphy-word').val("");
+	$(".users-giphy").empty();
+});
 
